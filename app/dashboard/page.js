1fa8c99bc31a6f679c58ff97c7a3793c6cd7f563
SlackinJack <41076523+SlackinJack@users.noreply.github.com>
2025-01-23 16:13:50 -0700

    add todos, refactor route

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,30 @@
 "use client";
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
 import "../styles.css";
 
-export default function Page() {
+export default function Page({ searchParams }) {
+    const router = useRouter();
+    const { userId, userToken } = use(searchParams);
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
+
+    useEffect(() => {
+        router.replace("/dashboard");
+        // TODO: delete after debug
+        alert("User authenticated: " + (userId != null && userToken != null).toString());
+    }, []);
+
+    // TODO: dynamically load names
+    const username = "Bob";
+    // TODO: dynamically load greeting
+    var greeting = "evening";
+    // TODO: dynamically load location
+    var location = "Alberta";
 
     return (
         <div className="h-full w-full flex flex-col">
@@ -17,40 +33,53 @@ export default function Page() {
 
             {/* Sidebar Toggle Button */}
             <button
-                className='fixed menu_button mt-7 -ml-[24px] pl-0 w-[24px] h-[72px] rounded-[10%]'
+                className="fixed menu_button mt-7 -ml-[24px] pl-0 w-[24px] h-[72px] rounded-[10%]"
                 onClick={toggleSidebar}
-                style={{ left: isSidebarOpen ? "350px" : "20px" }}
+                style={{
+                    left: isSidebarOpen ? "0" : "20px",
+                    display: isSidebarOpen ? "none" : "initial",
+                }}
             >&raquo;</button>
 
 
             {/* Sidebar */}
-            {isSidebarOpen && (
-                <div className="menu fixed w-[330px] h-full flex flex-col p-[24px]">
+                <div
+                    className="menu fixed w-[330px] h-full flex flex-col p-[24px]"
+                    style={{
+                        left: isSidebarOpen ? "0px": "-330px",
+                        boxShadow: isSidebarOpen ? "" : "none",
+                    }}
+                >
                     <h2 className="text-2xl font-bold mt-6 mb-12">Navigation</h2>
+                    <button
+                        className="absolute menu_close_button w-[48px] h-[48px] rounded-[50%] ml-[236px] mt-[16px]"
+                        onClick={toggleSidebar}
+                    >X</button>
                     <ul className="space-y-4">
+                        {/* TODO: this can be generated */}
                         <li>
-                            <a className='menu_item light_button py-3 px-4' href="#">Home</a>
+                            <a className="menu_item light_button block py-3 px-4 mr-1" href="#">Home</a>
                         </li>
                         <li>
-                            <a className='menu_item light_button py-3 px-4' href="#">Railway Capacity</a>
+                            <a className="menu_item light_button block py-3 px-4 mr-1" href="#">Railway Capacity</a>
                         </li> 
                         <li>
-                            <a className='menu_item light_button py-3 px-4' href="#">Weather Data</a>
+                            <a className="menu_item light_button block py-3 px-4 mr-1" href="#">Weather Data</a>
                         </li>
                         <li>
-                            <a className='menu_item light_button py-3 px-4' href="#">About Us</a>
+                            <a className="menu_item light_button block py-3 px-4 mr-1" href="#">About Us</a>
                         </li>
                     </ul>
                 </div>
-            )}
 
 
             {/* Main Content */}
             <div className="flex flex-col h-full w-full justify-stretch p-4">
-                <div className="p-4 pb-0">
-                    <h1 className="text-4xl font-bold">Good evening, $USER$</h1>
-                    <h2 className="text-base font-bold pb-8">Here is your overview for $LOCATION$</h2>
+                <div className="p-4 pb-auto">
+                    <h1 className="text-4xl font-bold">Good {greeting}, {username}!</h1>
+                    <h2 className="text-base font-bold pb-8">Here is your overview for {location}.</h2>
                 </div>
+                {/* TODO: use actual data */}
                 <div className="flex flex-col h-full w-full">
                     <div className="h-40 flex-auto m-2 flex flex-col justify-center text-center">
                         <div className="w-full flex flex-row justify-between text-center">
--- app/page.js
+++ app/page.js
@@ -1,27 +1,58 @@
-import Link from "next/link";
+"use client"
+import { useRouter } from "next/navigation";
 import "./styles.css";
 
 export default function Page() {
+    const router = useRouter();
+    const query = router.query;
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const input = event.target;
+        const e = input[0].value;
+        const p = input[1].value;
+
+        // TODO: auth on firebase
+        // TODO: (?) access token cookie
+        var success = true;
+        if (success) {
+            const userId = "user123";
+            const userToken = "7361380427";
+            router.push(`/dashboard?userId=${userId}&userToken=${userToken}`);
+        } else {
+            // TODO: better unauthorized page / notification
+            alert("Unauthorized");
+        }
+    }
     return (
         <div className="h-full w-full flex flex-col m-auto justify-center">
             <div className="m-12">
                 <div className="mb-12">
                     <h1 className="text-6xl font-bold">Welcome</h1>
-                    <h2 className="text-lg font-bold">Please sign-in to continue</h2>
+                    <h2 className="text-lg font-bold">Please sign-in to continue.</h2>
                 </div>
 
-                <div className="flex flex-col">
+                <form onSubmit={onSubmit} className="flex flex-col">
                     <h3 className="text-med font-bold pl-2 pb-2">E-mail:</h3>
-                    <input className="w-[80%] max-w-[400px] h-16 pl-6 mb-12"></input>
+                    <input
+                        type="email"
+                        placeholder="e-mail"
+                        className="w-[80%] max-w-[400px] h-16 pl-6 mb-12"
+                    />
+
                     <h3 className="text-med font-bold pl-2 pb-2">Password:</h3>
-                    <input className="w-[80%] max-w-[400px] h-16 pl-6 mb-12" type="password"></input>
-                    <Link
-                        className="h-16 w-[144px] dark_button text-lg font-bold pl-10 pt-4"
+                    <input
+                        type="password"
+                        placeholder="password"
+                        className="w-[80%] max-w-[400px] h-16 pl-6 mb-12" type="password"
+                    />
+
+                    <input
+                        type="submit"
+                        value="Authenticate"
+                        className="h-16 w-[160px] dark_button text-lg font-bold p-auto"
                         href="/railway-finance-management"
-                    >
-                        Submit
-                    </Link>
-                </div>
+                    />
+                </form>
             </div>
         </div>
     );
