c0ed0ca32d667d2b9c97c3efd5ab1ad4ceb4fd5a
SlackinJack <41076523+SlackinJack@users.noreply.github.com>
2025-01-23 01:30:25 -0700

    update login, dashboard page

--- app/page.js
+++ app/page.js
@@ -3,19 +3,25 @@ import "./styles.css";
 
 export default function Page() {
     return (
-        <div className="padded_body min-h-screen flex flex-col">
-            <div className="login_page_header">
-                <h1 className="login_page_greeting">Welcome</h1>
-                <p className="login_page_description">Please Sign-In to Continue</p>
-            </div>
-            <div className="body flex flex-col">
-                <h3 className="input_title">E-mail:</h3>
-                <input></input>
-                <h3 className="input_title">Password:</h3>
-                <input type="password"></input>
-                <Link href="/railway-finance-management" className="main_button login_page_submit_button">
-                    Submit
-                </Link>
+        <div className="h-full w-full flex flex-col m-auto justify-center">
+            <div className="m-12">
+                <div className="mb-12">
+                    <h1 className="text-6xl font-bold">Welcome</h1>
+                    <h2 className="text-lg font-bold">Please sign-in to continue</h2>
+                </div>
+
+                <div className="flex flex-col">
+                    <h3 className="text-med font-bold pl-2 pb-2">E-mail:</h3>
+                    <input className="w-[80%] max-w-[400px] h-16 pl-6 mb-12"></input>
+                    <h3 className="text-med font-bold pl-2 pb-2">Password:</h3>
+                    <input className="w-[80%] max-w-[400px] h-16 pl-6 mb-12" type="password"></input>
+                    <Link
+                        className="h-16 w-[144px] dark_button text-lg font-bold pl-10 pt-4"
+                        href="/railway-finance-management"
+                    >
+                        Submit
+                    </Link>
+                </div>
             </div>
         </div>
     );
--- app/railway-finance-management/page.js
+++ app/railway-finance-management/page.js
@@ -1,47 +1,96 @@
 "use client";
 import { useState } from "react";
+import Link from "next/link";
 import "../styles.css";
 
 export default function Page() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
 
     return (
-        <div className="min-h-screen flex">
+        <div className="h-full w-full flex flex-col">
+            {/* TODO: turn sidebar components into JS component for import */}
+
+
+            {/* Sidebar Toggle Button */}
+            <button
+                className='fixed menu_button mt-7 -ml-[24px] pl-0 w-[24px] h-[72px] rounded-[10%]'
+                onClick={toggleSidebar}
+                style={{ left: isSidebarOpen ? "350px" : "20px" }}
+            >&raquo;</button>
+
+
             {/* Sidebar */}
             {isSidebarOpen && (
-                <div className="menu w-1/4 h-full flex flex-col p-5">
-                    <h2 className="menu_title">Menu</h2>
+                <div className="menu fixed w-[330px] h-full flex flex-col p-[24px]">
+                    <h2 className="text-2xl font-bold mt-6 mb-12">Navigation</h2>
                     <ul className="space-y-4">
-                        <li><a className='menu_button' href="#">Home</a></li>
-                        <li><a className='menu_button' href="#">Railway Capacity</a></li>
-                        <li><a className='menu_button' href="#">Weather Data</a></li>
-                        <li><a className='menu_button' href="#">About Us</a></li>
+                        <li>
+                            <a className='menu_item light_button py-3 px-4' href="#">Home</a>
+                        </li>
+                        <li>
+                            <a className='menu_item light_button py-3 px-4' href="#">Railway Capacity</a>
+                        </li> 
+                        <li>
+                            <a className='menu_item light_button py-3 px-4' href="#">Weather Data</a>
+                        </li>
+                        <li>
+                            <a className='menu_item light_button py-3 px-4' href="#">About Us</a>
+                        </li>
                     </ul>
                 </div>
             )}
 
-            {/* Main Content */}
-            <div className="flex-1 flex flex-col justify-center items-center relative">
-                {/* Sidebar Toggle Button */}
-                <button
-                    className='menu_toggle_button'
-                    onClick={toggleSidebar}
-                    style={{
-                        left: isSidebarOpen ? "260px" : "20px", // Adjust position based on sidebar state
-                    }}
-                >
-                    <i
-                        className={`fa ${isSidebarOpen ? "fa-times" : "fa-bars"}`}
-                        style={{ fontSize: "1.5rem" }}
-                    />
-                </button>
 
-                <h1>Railway Management System</h1>
-                <p>Get capacity and live weather data for railway stations</p>
+            {/* Main Content */}
+            <div className="flex flex-col h-full w-full justify-stretch p-4">
+                <div className="p-4 pb-0">
+                    <h1 className="text-4xl font-bold">Good evening, $USER$</h1>
+                    <h2 className="text-base font-bold pb-8">Here is your overview for $LOCATION$</h2>
+                </div>
+                <div className="flex flex-col h-full w-full">
+                    <div className="h-40 flex-auto m-2 flex flex-col justify-center text-center">
+                        <div className="w-full flex flex-row justify-between text-center">
+                            <h1 className="text-xl font-bold text-left pl-1">Railway Map</h1>
+                            <Link className="text-xs dark_button px-3 py-2 mb-1" href="#">Expand Map</Link>
+                        </div>
+                        <div className="content_background h-full w-full flex flex-col justify-center">
+                            <h1>Railway Map</h1>
+                        </div>
+                    </div>
+                    <div className="h-10 flex-auto flex flex-row justify-stretch">
+                        <div className="flex-1 m-2 flex flex-col justify-center text-center">
+                            <div className="w-full flex flex-row justify-between text-center">
+                                <h1 className="text-base font-bold text-left pl-1">KPI #1</h1>
+                                <Link className="text-xs dark_button px-3 py-1 mb-1" href="#">Expand</Link>
+                            </div>
+                            <div className="content_background h-full w-full flex flex-col justify-center">
+                                <h1>KPI #1</h1>
+                            </div>
+                        </div>
+                        <div className="flex-1 m-2 flex flex-col justify-center text-center">
+                            <div className="w-full flex flex-row justify-between text-center">
+                                <h1 className="text-base font-bold text-left pl-1">KPI #2</h1>
+                                <Link className="text-xs dark_button px-3 py-1 mb-1" href="#">Expand</Link>
+                            </div>
+                            <div className="content_background h-full w-full flex flex-col justify-center">
+                                <h1>KPI #2</h1>
+                            </div>
+                        </div>
+                        <div className="flex-1 m-2 flex flex-col justify-center text-center">
+                            <div className="w-full flex flex-row justify-between text-center">
+                                <h1 className="text-base font-bold text-left pl-1">KPI #3</h1>
+                                <Link className="text-xs dark_button px-3 py-1 mb-1" href="#">Expand</Link>
+                            </div>
+                            <div className="content_background h-full w-full flex flex-col justify-center">
+                                <h1>KPI #3</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
