a6e13b141dd8dd1a473afccfe688d15da210a608
SlackinJack <41076523+SlackinJack@users.noreply.github.com>
2025-01-26 01:43:41 -0700

    add map, add graphs

--- app/components/kpi.js
+++ app/components/kpi.js
@@ -1,4 +1,7 @@
 import { usePathname } from "next/navigation";
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+import { PieChart } from "@mui/x-charts/PieChart";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 import "../styles.css";
 
@@ -10,26 +13,82 @@ export function KPI() {
             <div className="h-full w-full flex flex-row space-x-4 justify-stretch">
                 <div className="h-full w-full flex flex-col justify-center text-center">
                     { !isRouteOnExpandedPage &&
-                        <h1 className="text-xl font-bold text-left pl-1 mb-2">KPI #1</h1>
+                        <h1 className="text-xl font-bold text-left pl-1 mb-2">Capacity %</h1>
                     }
-                    <div className="content_background h-full w-full flex flex-col justify-center">
-                        <h1>Key Performance Indicator</h1>
+                    <div className="content_background h-full w-full flex flex-col justify-center text-center">
+                        <Gauge
+                            height={150}
+                            value={60}
+                            valueMax={100}
+                            startAngle={-90}
+                            endAngle={90}
+                            cornerRadius="50%"
+                            sx={(theme) => ({
+                                [`& .${gaugeClasses.valueText}`]: {
+                                    fontSize: 16,
+                                },
+                                [`& .${gaugeClasses.valueText} text`]: {
+                                    fill: "#ffffff",
+                                },
+                                [`& .${gaugeClasses.valueArc}`]: {
+                                    fill: '#ffffff',
+                                },
+                                [`& .${gaugeClasses.referenceArc}`]: {
+                                    fill: theme.palette.text.disabled,
+                                },
+                            })}
+                        />
                     </div>
                 </div>
                 <div className="h-full w-full flex flex-col justify-center text-center">
                     { !isRouteOnExpandedPage &&
-                        <h1 className="text-xl font-bold text-left pl-1 mb-2">KPI #2</h1>
+                        <h1 className="text-xl font-bold text-left pl-1 mb-2">Delays</h1>
                     }
-                    <div className="content_background h-full w-full flex flex-col justify-center">
-                        <h1>Key Performance Indicator</h1>
+                    <div className="content_background h-full w-full flex flex-col justify-center text-center">
+                        <PieChart
+                            height={200}
+                            colors={['#ffffff', '#000000', '#a0a0a0']}
+                            series={[
+                                {
+                                    data: [
+                                        { id: 0, value: 80, label: 'On-Time' },
+                                        { id: 1, value: 15, label: '< 5 mins' },
+                                        { id: 2, value: 5, label: '> 5 mins' },
+                                    ],
+                                },
+                            ]}
+                        />
                     </div>
                 </div>
                 <div className="h-full w-full flex flex-col justify-center text-center">
                     { !isRouteOnExpandedPage &&
-                        <h1 className="text-xl font-bold text-left pl-1 mb-2">KPI #3</h1>
+                        <h1 className="text-xl font-bold text-left pl-1 mb-2">Complaints</h1>
                     }
-                    <div className="content_background h-full w-full flex flex-col justify-center">
-                        <h1>Key Performance Indicator</h1>
+                    <div className="content_background h-full w-full flex flex-col justify-center text-center">
+                        <BarChart
+                            height={200}
+                            xAxis={[{
+                                scaleType: 'band',
+                                data: [
+                                    'jan',
+                                    'feb',
+                                    'mar',
+                                    'apr',
+                                    'may',
+                                    'jun',
+                                    'jul',
+                                    'aug',
+                                    'sep',
+                                    'oct',
+                                    'nov',
+                                    'dec',
+                                ]
+                            }]}
+                            series={[
+                                { data: [1, 2, 4, 5, 8, 1, 0, 2, 4, 6, 3, 2] },
+                            ]}
+                            colors={['#ffffff']}
+                        />
                     </div>
                 </div>
             </div>
--- app/components/map.js
+++ app/components/map.js
@@ -1,11 +1,126 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import React, { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 import "../styles.css";
 
 export function Map() {
     const pathname = usePathname();
+    const mapContainerRef = useRef();
+    const mapRef = useRef();
     var isRouteOnExpandedPage = (pathname === "/map");
+
+    useEffect(() => {
+        mapboxgl.accessToken = "pk.eyJ1IjoiaGFveXUtZ3VvIiwiYSI6ImNtNmRhZDJqNzBxOHIybW9wdzNzdmY5a20ifQ.8wDFOeZgYyCp-7ggCDA6Fw";
+        mapRef.current = new mapboxgl.Map({
+            container: 'map', // container ID
+            // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+            style: 'mapbox://styles/mapbox/dark-v11', // style URL
+            center: [-73.9709, 40.6712], // starting position [lng, lat]
+            zoom: 15.773 // starting zoom
+        });
+
+        const geojson = {
+            type: 'FeatureCollection',
+            features: [
+                {
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                        coordinates: [
+                            [-73.97003, 40.67264],
+                            [-73.96985, 40.67235],
+                            [-73.96974, 40.67191],
+                            [-73.96972, 40.67175],
+                            [-73.96975, 40.67154],
+                            [-73.96987, 40.67134],
+                            [-73.97015, 40.67117],
+                            [-73.97045, 40.67098],
+                            [-73.97064, 40.67078],
+                            [-73.97091, 40.67038],
+                            [-73.97107, 40.67011],
+                            [-73.97121, 40.66994],
+                            [-73.97149, 40.66969],
+                            [-73.97169, 40.66985],
+                            [-73.97175, 40.66994],
+                            [-73.97191, 40.66998],
+                            [-73.97206, 40.66998],
+                            [-73.97228, 40.67008]
+                        ],
+                    type: 'LineString'
+                    }
+                }
+            ]
+        };
+
+        mapRef.current.on('load', () => {
+            mapRef.current.addSource('line', {
+                type: 'geojson',
+                data: geojson
+            });
+
+            mapRef.current.addLayer({
+                type: 'line',
+                source: 'line',
+                id: 'line-background',
+                paint: {
+                    'line-color': 'yellow',
+                    'line-width': 6,
+                    'line-opacity': 0.4
+                }
+            });
+
+            mapRef.current.addLayer({
+                type: 'line',
+                source: 'line',
+                id: 'line-dashed',
+                paint: {
+                    'line-color': 'yellow',
+                    'line-width': 6,
+                    'line-dasharray': [0, 4, 3]
+                }
+            });
+
+            const dashArraySequence = [
+                [0, 4, 3],
+                [0.5, 4, 2.5],
+                [1, 4, 2],
+                [1.5, 4, 1.5],
+                [2, 4, 1],
+                [2.5, 4, 0.5],
+                [3, 4, 0],
+                [0, 0.5, 3, 3.5],
+                [0, 1, 3, 3],
+                [0, 1.5, 3, 2.5],
+                [0, 2, 3, 2],
+                [0, 2.5, 3, 1.5],
+                [0, 3, 3, 1],
+                [0, 3.5, 3, 0.5]
+            ];
+
+            let step = 0;
+
+            function animateDashArray(timestamp) {
+                const newStep = parseInt((timestamp / 50) % dashArraySequence.length);
+
+                if (newStep !== step) {
+                    mapRef.current.setPaintProperty(
+                        'line-dashed',
+                        'line-dasharray',
+                        dashArraySequence[step]
+                    );
+                    step = newStep;
+                }
+
+                requestAnimationFrame(animateDashArray);
+            }
+
+            animateDashArray(0);
+        });
+    }, []);
+
     return (
         <>
             <div className="h-full w-full flex-auto flex flex-col justify-center text-center">
@@ -16,7 +131,7 @@ export function Map() {
                     </div>
                 }
                 <div className="content_background h-full w-full flex flex-col justify-center">
-                    <h1>Railway Map</h1>
+                    <div id="map" ref={mapContainerRef} className="h-full w-full"></div>
                 </div>
             </div>
         </>
